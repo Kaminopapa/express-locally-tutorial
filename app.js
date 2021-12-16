0d613f23bb1fa47f28186b2ceeeb34063e5719cd
CPSC2150 <84880870+Kaminopapa@users.noreply.github.com>
2021-12-16 04:36:56 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var helmet = require('helmet');
 var app = express();
 //set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://cpsc2600:0062cspc@cluster0.xm5ii.mongodb.net/localLibrary?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cpsc2600:0062cspc@cluster0.xm5ii.mongodb.net/localLibrary?retryWrites=true&w=majority';
+var mongoDB=process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB,{useNewUrlParser: true,useUnifiedTopology:true});
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'MongoDB connection error:'));
